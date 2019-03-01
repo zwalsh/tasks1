b3f0d070328d771d28cfbb16d0c2c1f3c5290c40
root <root@vultr.guest>
2019-03-01 03:08:12 +0000

    Deploy stuff

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,18 @@
 use Mix.Config
 
+get_secret = fn name ->
+  # Secret generation hack by Nat Tuck for CS4550
+  # This function is dedicated to the public domain.
+  base = Path.expand("~/.config/phx-secrets")
+  File.mkdir_p!(base)
+  path = Path.join(base, name)
+  unless File.exists?(path) do
+    secret = Base.encode16(:crypto.strong_rand_bytes(32))
+    File.write!(path, secret)
+  end
+  String.trim(File.read!(path))
+end
+
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
@@ -10,9 +23,14 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tasks1, Tasks1Web.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true,
+  root: ".",
+  version: Application.spec(:phoenix_distillery, :vsn),
+  http: [:inet6, port: {:system, "PORT"}],
+  url: [host: "tasks1.zachwal.sh", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: get_secret.("key_base"),
+  password: get_secret.("tasks1")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -67,5 +85,12 @@ config :logger, level: :info
 # See the releases documentation accordingly.
 
 # Finally import the config/prod.secret.exs which should be versioned
-# separately.
-import_config "prod.secret.exs"
+#
+
+# Configure your database
+config :tasks1, Tasks1.Repo,
+  username: "tasks1",
+  password: "NfFKPy9nocHo5Nh9hiU3zYtJ32ABN9",
+  database: "tasks1",
+  hostname: "localhost",
+  pool_size: 10 
